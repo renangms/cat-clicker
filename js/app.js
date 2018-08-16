21c8c3638ac8edec380e7b99e0e178a51526e830
Renan Machado <renangms@gmail.com>
2018-08-16 10:56:24 -0500

    Add 2nd cat

--- js/app.js
+++ js/app.js
@@ -1,11 +1,25 @@
-var $catElem = $('#cat')
+var clickCount1 = 0;
+var clickCount2 = 0;
 
-var clickCount = 0;
+var $img1 = $('#cat1 img')
+var $img2 = $('#cat2 img')
 
-function catClicked() {
-    var $countElem = $('#click-count');
-    clickCount += 1;
-    $countElem.text('Click count: ' + clickCount);
+$(document).ready(function() {
+    $('#cat1').append('<figcaption>Cat 1</figcaption>');
+    $('#cat2').append('<figcaption>Cat 2</figcaption>');
+});
+
+function cat1Clicked() {
+    var $countElem = $('#click-count1');
+    clickCount1 += 1;
+    $countElem.text('Click count: ' + clickCount1);
 };
 
-$catElem.click(catClicked);
+function cat2Clicked() {
+    var $countElem = $('#click-count2');
+    clickCount2 += 1;
+    $countElem.text('Click count: ' + clickCount2);
+};
+
+$img1.click(cat1Clicked);
+$img2.click(cat2Clicked);

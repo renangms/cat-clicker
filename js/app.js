7ad148db145222750a766359e21f62b939f2e805
Renan Machado <renangms@gmail.com>
2018-08-25 18:46:21 -0500

    Add MVO pattern

--- js/app.js
+++ js/app.js
@@ -1,75 +1,119 @@
-var cats = [
-    {
-        id: 'cat1',
-        img: 'img/cat1.jpg',
-        name: 'Cat 1',
-        clickCount: 0
-    },
-    {
-        id: 'cat2',
-        img: 'img/cat2.jpg',
-        name: 'Cat 2',
-        clickCount: 0
-    },
-    {
-        id: 'cat3',
-        img: 'img/cat3.jpg',
-        name: 'Cat 3',
-        clickCount: 0
-    },
-    {
-        id: 'cat4',
-        img: 'img/cat4.jpg',
-        name: 'Cat 4',
-        clickCount: 0
-    },
-    {
-        id: 'cat5',
-        img: 'img/cat5.jpg',
-        name: 'Cat 5',
-        clickCount: 0
-    },
-]
+var model = {
+    selectedCat: null,
+    cats: [
+        {
+            id: 'cat1',
+            img: 'img/cat1.jpg',
+            name: 'Cat 1',
+            clickCount: 0
+        },
+        {
+            id: 'cat2',
+            img: 'img/cat2.jpg',
+            name: 'Cat 2',
+            clickCount: 0
+        },
+        {
+            id: 'cat3',
+            img: 'img/cat3.jpg',
+            name: 'Cat 3',
+            clickCount: 0
+        },
+        {
+            id: 'cat4',
+            img: 'img/cat4.jpg',
+            name: 'Cat 4',
+            clickCount: 0
+        },
+        {
+            id: 'cat5',
+            img: 'img/cat5.jpg',
+            name: 'Cat 5',
+            clickCount: 0
+        }
+    ]
+};
 
-var selectedCat = null;
+var octopus = {
+    init: function () {
+        model.selectedCat = model.cats[0];
+        catListView.init();
+        catView.init();
+    },
 
-function getCatById(id) {
-    for (var i = 0; i < cats.length; i++) {
-        var cat = cats[i];
+    getCatById: function (id) {
 
-        if (cat.id == id) {
-           return cat;
-       } 
-    }
+        var cats = model.cats;
 
-    return null;
+        for (var i = 0; i < cats.length; i++) {
+            var cat = cats[i];
+
+            if (cat.id == id) {
+                return cat;
+            }
+        }
+
+        return null;
+    },
+
+    getCats: function () {
+        return model.cats;
+    },
+
+    getSelectedCat: function () {
+        return model.selectedCat;
+    },
+
+    updateCat: function (id) {
+        model.selectedCat = this.getCatById(id);
+        catView.render();
+    },
+
+    incrementClickCounter: function () {
+        model.selectedCat.clickCount++;
+        catView.render();
+    },
 }
 
+var catListView = {
+    init: function () {
+        this.catListElem = $('#cat-list');
 
-$(function() {
-    var catListElem = $('#cat-list');
+        this.render();
+    },
 
-    for(var i = 0; i < cats.length; i++) {
-        catListElem.append('<li id="' + cats[i].id + '">' + cats[i].name + '</li>')
+    render: function () {
+        var cats = octopus.getCats();
+
+        for (var i = 0; i < cats.length; i++) {
+            this.catListElem.append('<li id="' + cats[i].id + '">' + cats[i].name + '</li>')
+        }
+
+        $("#cat-list li").click(function (e) {
+            var id = e.target.id;
+            octopus.updateCat(id);
+        });
     }
-});
+};
 
-$(function() {
-    $("#cat-list li").click(function(e) {
-        var id = e.target.id;
+var catView = {
+    init: function () {
+        this.catImg = $("#cat img");
 
-        selectedCat = getCatById(id);
+        this.catImg.click(function (e) {
+            octopus.incrementClickCounter();
+        });
 
-        $('#cat img').attr('src', selectedCat.img);
-        $('#cat figcaption').text(selectedCat.name);
+        this.render();
+    },
+
+    render: function () {
+        var selectedCat = octopus.getSelectedCat();
+
+        this.catImg.attr('src', selectedCat.img);
+        $('#cat h2').text(selectedCat.name);
         $('#click-count').text('Click count: ' + selectedCat.clickCount);
-    });
-});
+    }
+};
 
-$(function() {
-    $("#cat img").click(function(e) {
-        selectedCat.clickCount += 1;
-        
-        $('#click-count').text('Click count: ' + selectedCat.clickCount);
-    });
-});
+octopus.init();
